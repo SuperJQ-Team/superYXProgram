3d1f1d1db5fad4d8b11e45969d45e3916b1183b9
L-Bromine <lishaofan_31415926@163.com>
2024-07-10 13:05:16 +0800

    添加了购物车校对后端

--- SuperPlus7Program-spring/src/main/java/com/etoak/java/service/IGoodsService.java
+++ SuperPlus7Program-spring/src/main/java/com/etoak/java/service/IGoodsService.java
@@ -1,10 +1,11 @@
 package com.etoak.java.service;
 
+import com.baomidou.mybatisplus.extension.service.IService;
 import com.etoak.java.entity.Goods;
 
 import java.util.List;
 
-public interface IGoodsService {
+public interface IGoodsService extends IService<Goods> {
     List<Goods> list();
     List<Goods> list(Long user_id);
     int add(Goods goods);
--- SuperPlus7Program-spring/src/main/java/com/etoak/java/service/ILoginService.java
+++ SuperPlus7Program-spring/src/main/java/com/etoak/java/service/ILoginService.java
@@ -1,9 +1,10 @@
 package com.etoak.java.service;
 
+import com.baomidou.mybatisplus.extension.service.IService;
 import com.etoak.java.entity.SysUser;
 
 
-public interface ILoginService {
+public interface ILoginService extends IService<SysUser> {
     SysUser login(String username, String password);
     int signup(String username, String password, String role);
     int writeoff(String username, String password);
